d60a00ee6b37a9cd6eb35ea2e8ccde8dfe14d04b
lydiolectal <lydiding@gmail.com>
2018-04-03 16:21:04 -0400

    refactored into functions and added scraping for kitchn recipes

--- recipe_scraper.py
+++ recipe_scraper.py
@@ -2,62 +2,94 @@
 # for playing with beautiful soup.
 # lol @ the filename.
 
+# to scrape (for now):
+# allrecipes, kitchn, chow, epicurious, simply recipes, yummly.
+
 from bs4 import BeautifulSoup
 from urllib import request
 # regex library
 import re
 
-with request.urlopen("http://www.geniuskitchen.com/recipe/best-banana-bread-2886") as f:
-    # constructor takes a string and parser choice as args.
-    soup = BeautifulSoup(f, "html.parser")
+"""
+problems
+- why so slow?
+- in genius kitchen, sometimes there's no serves or no yields value;
+how to handle exception and return a dictionary with empty list?
+- clean " from ingredients list in an efficient manner ([1:-1]string splicing?)
+"""
 
-# print(soup.prettify())fdfds
+def kitchn(url):
+    with request.urlopen(url) as f:
+        # constructor takes a string and parser choice as args.
+        soup = BeautifulSoup(f, "html.parser")
 
-# get ingredients from 'extras' div.
-# TODO: wrangle this string mess into an actual python list of ingredients.
-ingredients = soup.find("div", class_ = "extras").input["value"]
-# regex defining everything that falls between double "", not including other ".
-pattern = re.compile('"[^"]*"')
-cleaned_ingredients = pattern.findall(ingredients)
-# print(cleaned_ingredients)
+    pre_steps = soup.find("div", class_ = "PostRecipeInstructionGroup")
+    steps = [string for string in pre_steps.stripped_strings]
 
-# time, yield, servings - need to ignore header and get to text.
-# TODO: make this less clunky.
-try:
-    timeTag = soup.find("td", class_ = "time")
-    time = []
-    for string in timeTag.stripped_strings:
-        time.append(string)
-    print(time)
-except AttributeError:
-    pass
+    pre_ingredients = soup.find_all("li", class_ = "PostRecipeIngredientGroup__ingredient")
+    ingredients = []
+    for pre_ingredient in pre_ingredients:
+        ingredient = [item for item in pre_ingredient.stripped_strings]
+        ingredients.append(" ".join(ingredient))
 
-try:
-    yieldsTag = soup.find("td", class_ = "yield")
-    yields = []
-    for string in yieldsTag.stripped_strings:
-        yields.append(string)
-    print(yields)
-except AttributeError:
-    pass
+    pre_yields = soup.find("p", class_ = "PostRecipe__yield")
+    yields = " ".join([string for string in pre_yields.stripped_strings])
 
-try:
-    servesTag = soup.find("td", class_ = "servings")
-    serves = []
-    for string in servesTag.stripped_strings:
-        serves.append(string)
-    print(serves)
-except AttributeError:
-    pass
+    return {"steps": steps, "ingredients": ingredients, "yields": yields}
 
-# find the ol tag containing directions.
-# extract text from li elements that only contain text.
-directions = soup.find("ol") # assumes there's only 1 ol.
-# otherDirections = soup.select(".directions-inner")
-# print(otherDirections)
+def genius_kitchen(url):
 
-# extract text from these divs and clean.
-steps = []
-for string in directions.stripped_strings:
-    steps.append(string)
-print(steps[:-1])
+    with request.urlopen(url) as f:
+        # constructor takes a string and parser choice as args.
+        soup = BeautifulSoup(f, "html.parser")
+
+    # dictionary including ingredients (array), time (string), yield (string),
+    # servings (string), and recipe steps (array).
+    # get ingredients from 'extras' div.
+
+    recipe = {}
+    pre_ingredients = soup.find("div", class_ = "extras").input["value"]
+    # regex defining everything that falls between double "", not including other ".
+    pattern = re.compile('"[^"]*"')
+    ingredients = pattern.findall(pre_ingredients)
+    recipe["ingredients"] = ingredients
+
+    # time, yield, servings - need to ignore header and get to text.
+    try:
+        timeTag = soup.find("td", class_ = "time")
+        time = [string for string in timeTag.stripped_strings]
+
+        yieldsTag = soup.find("td", class_ = "yield")
+        yields = [string for string in yieldsTag.stripped_strings]
+
+        servesTag = soup.find("td", class_ = "servings")
+        serves = [string for string in servesTag.stripped_strings]
+
+    except AttributeError:
+        pass
+
+    recipe["time"] = [] if not(time) else time[1:]
+    recipe["yields"] = [] if not(yields) else yields[1:]
+    recipe["serves"] = [] if not(serves) else serves[1:]
+
+    # find the ol tag containing directions.
+    # extract text from li elements that only contain text.
+    directions = soup.find("ol") # assumes there's only 1 ol.
+
+    # extract text from these divs and clean.
+    steps = []
+    for string in directions.stripped_strings:
+        steps.append(string)
+    steps = steps[:-1]
+
+    recipe["steps"] = steps
+    return recipe
+
+if __name__ == "__main__":
+
+    # genius kitchen
+    # genius_kitchen("http://www.geniuskitchen.com/recipe/best-banana-bread-2886")
+
+    # print(genius_kitchen("http://www.geniuskitchen.com/recipe/pancakes-25690"))
+    # simply_recipes("https://www.allrecipes.com/recipe/236064/our-best-cheesecake/?internalSource=streams&referringId=276&referringContentType=recipe%20hub&clickId=st_recipes_mades")
+    print(kitchn("https://www.thekitchn.com/cold-weather-recipe-white-chicken-chili-recipes-from-the-kitchn-181533"))
